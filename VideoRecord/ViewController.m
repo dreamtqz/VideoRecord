0ff975f0fbceefe05a8df094c1d4cc2cd35f3661
汤庆泽 <tangqingze@kdnet.net>
2017-08-04 15:33:42 +0800

    Initial Commit

---
+++ VideoRecord/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VideoRecord
+//
+//  Created by 汤庆泽 on 17/8/4.
+//  Copyright © 2017年 海南凯迪网络有限公司广州分公司. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ VideoRecord/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoRecord
+//
+//  Created by 汤庆泽 on 17/8/4.
+//  Copyright © 2017年 海南凯迪网络有限公司广州分公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
